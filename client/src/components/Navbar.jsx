cd29a6019e866f4264d2154c2e7a524c20c38740
Ismail Mabrouki <39934485+IsmailMabrouki@users.noreply.github.com>
2023-11-24 20:10:09 +0100

    merge client and server and add mongo db

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import { AiOutlineLogin } from "react-icons/ai";
 import "../styles/Navbar.css";
+
 const Navbar = () => {
   const navbar = useRef(null);
+
   useEffect(() => {
     document.addEventListener("scroll", function (e) {
       if (window.pageYOffset === 0) {
@@ -14,14 +17,18 @@ const Navbar = () => {
       }
     });
   }, []);
+
   return (
     <nav ref={navbar}>
       <div className="nav-items">
         <div className="nav-item">Menu</div>
         <div className="nav-item logo">PharmaTree</div>
         <div className="nav-item nav-item--icon">
-          <span>Login</span>
-          <AiOutlineLogin/>
+          {/* Use Link to navigate to the login page */}
+          <Link to="/login">
+            <span>Login</span>
+            <AiOutlineLogin />
+          </Link>
         </div>
       </div>
     </nav>
@@ -29,3 +36,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
